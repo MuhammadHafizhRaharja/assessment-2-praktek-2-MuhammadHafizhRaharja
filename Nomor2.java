f5c916823851414ac93f04efeb21fefead6cc5b5
Hafizh <110800141+MuhammadHafizhRaharja@users.noreply.github.com>
2024-12-12 09:57:17 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -1,13 +1,61 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Nomor2 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
-        /* Pekerjaan anda mulai dari sini */
+        int[] hargaBuku = {350000, 450000, 500000, 95000};
+        ArrayList<Integer> hargaBelanja = new ArrayList<>();
 
-        /* Pekerjaan anda berakhir sini */
+        System.out.println("Daftar Buku:");
+        for (int i = 0; i < hargaBuku.length; i++) {
+            System.out.println((i + 1) + ". " + getNamaBuku(i) + " " + hargaBuku[i]);
+        }
+
+        while (true) {
+            System.out.print("Masukan pilihan buku [1/2/3/4]:");
+            int pilihan = input.nextInt();
+            hargaBelanja.add(hargaBuku[pilihan - 1]);
+            System.out.print("Input lagi (Y/N)?:");
+            String jawaban = input.next();
+            if (jawaban.equalsIgnoreCase("N")) {
+                break;
+            }
+        }
+
+        int totalHarga = hitungTotalHarga(hargaBelanja);
+        int voucherBelanja = hitungVoucherBelanja(totalHarga);
+
+        System.out.println("Total voucher belanja = " + voucherBelanja);
+        System.out.println("Total yang harus dibayar = " + (totalHarga - voucherBelanja));
 
         System.out.println(Nomor2.class.getDeclaredMethods().length); 
+    }
+
+    public static String getNamaBuku(int index) {
+        String[] namaBuku = {"Sherlock Holmes", "Arsene Lupin", "Game Of Throne", "Psychology Of Money"};
+        return namaBuku[index];
+    }
+
+    public static int hitungTotalHarga(ArrayList<Integer> hargaBelanja) {
+        int totalHarga = 0;
+        for (int harga : hargaBelanja) {
+            totalHarga += harga;
+        }
+        return totalHarga;
+    }
+
+    public static int hitungVoucherBelanja(int totalHarga) {
+        if (totalHarga < 200000) {
+            return 0;
+        } else if (totalHarga >= 200000 && totalHarga <= 500000) {
+            return 50000;
+        } else if (totalHarga >= 500000 && totalHarga <= 750000) {
+            return 100000;
+        } else if (totalHarga >= 750000 && totalHarga <= 1000000) {
+            return 150000;
+        } else {
+            return 200000;
+        }
     }
 }
