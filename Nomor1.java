883af1336cad46008f31227403d2beb131030a1e
Hafizh <110800141+MuhammadHafizhRaharja@users.noreply.github.com>
2024-12-12 09:51:28 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -5,19 +5,18 @@ public class Nomor1 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
-        /* Pekerjaan anda mulai dari sini */
         for (int i = 0; i <= N; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
-                System.out.print("FizzBuzz ");
+                System.out.print("FizzBuzz");
             } else if (i % 3 == 0) {
-                System.out.print("Fizz ");
+                System.out.print("Fizz");
             } else if (i % 5 == 0) {
-                System.out.print("Buzz ");
+                System.out.print("Buzz");
             } else {
-                System.out.print(i + " ");
+                System.out.print(i);
             }
+            System.out.print(" ");
         }
-        /* Pekerjaan anda berakhir sini */
 
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
     }
