86cec380da6395567d277cb48316960d3f3c1d3f
Hafizh <110800141+MuhammadHafizhRaharja@users.noreply.github.com>
2024-12-12 10:16:23 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -9,7 +9,7 @@ public class Nomor1 {
     }
 
     public static void fizzBuzz(int n) {
-        for (int i = 0; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.print("FizzBuzz ");
             } else if (i % 3 == 0) {
