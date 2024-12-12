37030debfbb14853b943ada0124b9f618b0fcd4c
Hafizh <110800141+MuhammadHafizhRaharja@users.noreply.github.com>
2024-12-12 10:26:27 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -4,47 +4,56 @@ import java.util.Scanner;
 public class Nomor2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-        // Daftar buku
-        String[] buku = {"Sherlock Holmes", "Arsene Lupin", "Game of Throne", "Psychology of Money"};
-        int[] harga = {350000, 450000, 500000, 95000};
-
-        // Tampilkan daftar buku
-        System.out.println("Daftar Buku:");
-        for (int i = 0; i < buku.length; i++) {
-            System.out.println((i + 1) + ". " + buku[i] + " " + harga[i]);
-        }
-
-        // Tampung harga belanja user ke dalam ArrayList
         ArrayList<Integer> belanja = new ArrayList<>();
-        int totalHarga = 0;
-        while (true) {
-            System.out.print("Masukan pilihan buku [1/2/3/4]:");
+        int[] hargaBuku = {350000, 450000, 500000, 95000};
+        String[] daftarBuku = {"Sherlock Holmes", "Arsene Lupin", "Game of Throne", "Psychology of Money"};
+
+        System.out.println("Daftar Buku:");
+        for (int i = 0; i < daftarBuku.length; i++) {
+            System.out.println((i + 1) + ". " + daftarBuku[i] + " " + hargaBuku[i]);
+        }
+
+        char lagi;
+        do {
+            System.out.print("Masukan pilihan buku [1/2/3/4]: ");
             int pilihan = input.nextInt();
-            belanja.add(harga[pilihan - 1]);
-            totalHarga += harga[pilihan - 1];
-            System.out.print("Input lagi (Y/N)?:");
-            String lagi = input.next();
-            if (lagi.equalsIgnoreCase("N")) {
-                break;
+
+            if (pilihan >= 1 && pilihan <= 4) {
+                belanja.add(hargaBuku[pilihan - 1]);
+            } else {
+                System.out.println("Pilihan tidak valid.");
             }
-        }
 
-        // Hitung voucher belanja
-        int voucher = 0;
-        if (totalHarga >= 200000 && totalHarga < 500000) {
-            voucher = 50000;
-        } else if (totalHarga >= 500000 && totalHarga < 750000) {
-            voucher = 100000;
-        } else if (totalHarga >= 750000 && totalHarga < 1000000) {
-            voucher = 150000;
-        } else if (totalHarga >= 1000000) {
-            voucher = 200000;
-        }
+            System.out.print("Input lagi (Y/N)?: ");
+            lagi = input.next().charAt(0);
+        } while (lagi == 'Y' || lagi == 'y');
 
-        // Tampilkan output
-        System.out.println("Total voucher belanja : " + voucher);
-        System.out.println("Total yang harus dibayar : " + (totalHarga - voucher));
-        System.out.println(3);
+        int totalHarga = hitungTotalHarga(belanja);
+        int voucher = hitungVoucher(totalHarga);
+
+        System.out.println("Total voucher belanja = " + voucher);
+        System.out.println("Total yang harus dibayar = " + totalHarga);
+    }
+
+    public static int hitungTotalHarga(ArrayList<Integer> belanja) {
+        int total = 0;
+        for (int harga : belanja) {
+            total += harga;
+        }
+        return total;
+    }
+
+    public static int hitungVoucher(int totalHarga) {
+        if (totalHarga < 200000) {
+            return 0;
+        } else if (totalHarga <= 500000) {
+            return 50000;
+        } else if (totalHarga <= 750000) {
+            return 100000;
+        } else if (totalHarga <= 1000000) {
+            return 150000;
+        } else {
+            return 200000;
+        }
     }
 }
