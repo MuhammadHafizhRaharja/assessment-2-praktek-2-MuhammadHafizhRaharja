f40e50ac6ea99512cc9e3bbd2744bd4012d61283
Hafizh <110800141+MuhammadHafizhRaharja@users.noreply.github.com>
2024-12-12 10:10:37 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,25 +1,24 @@
 import java.util.Scanner;
 
 public class Nomor1 {
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
-        for (int i = 0; i <= N; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.print("FizzBuzz");
-            } else if (i % 3 == 0) {
-                System.out.print("Fizz");
-            } else if (i % 5 == 0) {
-                System.out.print("Buzz");
-            } else {
-                System.out.print(i);
-            }
-            System.out.print(" ");
-        }
-
-        System.out.println(Nomor1.class.getDeclaredMethods().length); 
+        fizzBuzz(N);
+        System.out.println(Nomor1.class.getDeclaredMethods().length);
     }
 
+    public static void fizzBuzz(int n) {
+        for (int i = 0; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("FizzBuzz ");
+            } else if (i % 3 == 0) {
+                System.out.print("Fizz ");
+            } else if (i % 5 == 0) {
+                System.out.print("Buzz ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+    }
 }
-    
