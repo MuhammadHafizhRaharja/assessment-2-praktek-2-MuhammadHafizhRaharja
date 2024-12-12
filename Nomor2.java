46c0051283d5d090b8b43c6ed662e61003aa671a
Hafizh <110800141+MuhammadHafizhRaharja@users.noreply.github.com>
2024-12-12 10:31:41 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -10,18 +10,19 @@ public class Nomor2 {
 
         System.out.println("Daftar Buku:");
         for (int i = 0; i < daftarBuku.length; i++) {
-            System.out.println((i + 1) + ". " + daftarBuku[i] + " " + hargaBuku[i]);
+            System.out.printf("%d. %s - Rp%d%n", (i + 1), daftarBuku[i], hargaBuku[i]);
         }
 
         char lagi;
         do {
-            System.out.print("Masukan pilihan buku [1/2/3/4]: ");
+            System.out.print("Masukkan pilihan buku [1/2/3/4]: ");
             int pilihan = input.nextInt();
 
             if (pilihan >= 1 && pilihan <= 4) {
                 belanja.add(hargaBuku[pilihan - 1]);
+                System.out.println(daftarBuku[pilihan - 1] + " berhasil ditambahkan ke keranjang.");
             } else {
-                System.out.println("Pilihan tidak valid.");
+                System.out.println("Pilihan tidak valid. Silakan pilih antara 1 hingga 4.");
             }
 
             System.out.print("Input lagi (Y/N)?: ");
@@ -31,9 +32,14 @@ public class Nomor2 {
         int totalHarga = hitungTotalHarga(belanja);
         int voucher = hitungVoucher(totalHarga);
 
-        System.out.println("Total voucher belanja : " + voucher);
-        System.out.println("Total yang harus dibayar : " + totalHarga);
-        System.out.println(belanja.size());
+        System.out.println("\nDetail Belanja:");
+        for (int i = 0; i < belanja.size(); i++) {
+            System.out.printf("Buku %d: Rp%d%n", (i + 1), belanja.get(i));
+        }
+
+        System.out.println("\nTotal harga belanja: Rp" + totalHarga);
+        System.out.println("Total voucher belanja: Rp" + voucher);
+        System.out.println("Total yang harus dibayar: Rp" + (totalHarga - voucher));
     }
 
     public static int hitungTotalHarga(ArrayList<Integer> belanja) {
